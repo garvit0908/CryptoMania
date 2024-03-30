44912f876b38fbf9787bf09a49cab105621adf2b
Garvit Mukhija <mukhijagarvit@gmail.com>
2024-03-30 11:13:06 +0530

    Established Routes

--- CryptoMania/src/App.jsx
+++ CryptoMania/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Home from './Pages/Home'
+import Home from './Components/Home'
+import CryptoCurrencies from './Components/CryptoCurrencies'
+import Exchanges from './Components/Exchanges'
+import News from './Components/CryptoNews'
 import Navbar from './Components/Navbar'
 import NavExample from './Components/NavExample'
+import { BrowserRouter,Route,Routes} from 'react-router-dom'
+import CryptoNews from './Components/CryptoNews'
 
 
 //There will be hell lot of components and u have to plan that
@@ -12,10 +17,20 @@ function App() {
 
   return (
     <>
-    {/* <h1 className='bg-green-500 p-4 rounded-xl'>Hello</h1> */}
-    {/* <Navbar /> */}
-    {/* <Home /> */}
-    {/* <NavExample/> */}
+    <div>
+      <Navbar/>
+    </div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/cryptocurrencies" element={<CryptoCurrencies/>}/>
+          <Route path="/cryptonews" element={<CryptoNews/>}/>
+          <Route path="/exchanges" element={<Exchanges/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+
     </>
   )
 }
--- CryptoMania/src/Components/Exchanges.jsx
+++ CryptoMania/src/Components/Exchanges.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Exchanges() {
   return (
     <div>
-      
+      Exchanges
     </div>
   )
 }
